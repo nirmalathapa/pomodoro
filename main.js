df379752970f85c4a780fa792d364d8d9ffa3c5e
nirmala <nirmala@surf.net.np>
2018-04-14 04:35:31 +0200

    add global and local varibales

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-var timeInt, minutes, seconds;	
 //click the button 	
 var button = document.getElementsByClassName('start');	
-button[0].addEventListener('click', function(){	
-    var display = document.getElementsByTagName('h2');	
-    display[0].textContent = new Date();	
+var display = document.getElementsByTagName('h2');	
+
+var timer = new Date().getMinutes();
+console.log(timer);
+
+
+button[0].addEventListener('click', function(){	    
+    var counter = setInterval(function(){
+        var now = new Date().getTime()
+    },1000);
 });	
  //start coundown 25:00	
      //time decreases	
