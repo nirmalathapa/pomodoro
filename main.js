7e2694997a55e826a04ece66b703e413679be920
nirmala <nirmala@surf.net.np>
2018-04-16 19:17:25 +0200

    add counter to start button

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 //click the button 	
 var button = document.getElementsByClassName('start');	
-var display = document.getElementsByTagName('h2');	
-
-var timer = new Date().getMinutes();
-console.log(timer);
+var time = document.querySelector('h2');
+var countTime = new Date().getTime();
 
 
 button[0].addEventListener('click', function(){	    
+    
     var counter = setInterval(function(){
-        var now = new Date().getTime()
+        var now = new Date().getTime();
+        var diff = now - countTime;
+        var min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        var sec = Math.floor((diff % (1000 * 60)) / (1000));
+        document.querySelector('h2').innerHTML = min + sec;
+
     },1000);
+    
 });	
  //start coundown 25:00	
      //time decreases	
