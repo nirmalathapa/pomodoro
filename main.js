18a29252d8c50d45b5ff44df1ceec93c34e02180
nirmala <nirmala@surf.net.np>
2018-04-17 19:00:18 +0200

    edit js variable

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
 //click the button 	
 var button = document.getElementsByClassName('start');	
-var time = document.querySelector('h2');
-var countTime = new Date().getTime();
-
+var display = document.getElementsByClassName('display');
+var timer = 25;
+var timerBreak = 5;
 
 button[0].addEventListener('click', function(){	    
     
     var counter = setInterval(function(){
-        var now = new Date().getTime();
-        var diff = now - countTime;
-        var min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        var sec = Math.floor((diff % (1000 * 60)) / (1000));
-        document.querySelector('h2').innerHTML = min + sec;
-
+        
     },1000);
-    
+    clearInterval(counter);
 });	
- //start coundown 25:00	
-     //time decreases	
-     //becomes 00:00
+ 
